7fcf3c9b7668d591dc901b0741636cbcf0444ce8
Luke Bennett <hello@lukebennett.com.au>
2023-06-08 09:55:45 +1000

    Add explainer text above links and posts

--- apps/web/src/app/(site)/links/page.tsx
+++ apps/web/src/app/(site)/links/page.tsx
@@ -1,3 +1,5 @@
+import { default as Link } from 'next/link';
+
 import { reader } from '~/keystatic/reader';
 
 export default async function Page() {
@@ -5,6 +7,10 @@ export default async function Page() {
 	return (
 		<div className="prose dark:prose-invert">
 			<h1>Links</h1>
+			<p>
+				Interesting links with limited commentary. For longer form content,{' '}
+				<Link href="/posts">check out the posts section</Link> instead.
+			</p>
 			<ul>
 				{links.map((link) => (
 					<li key={link.slug}>
--- apps/web/src/app/(site)/posts/page.tsx
+++ apps/web/src/app/(site)/posts/page.tsx
@@ -1,13 +1,16 @@
-import { clsx } from 'clsx';
+import { default as Link } from 'next/link';
 
 import { reader } from '~/keystatic/reader';
-import { h1Classes } from '~/lib/classes';
 
 export default async function Page() {
 	const posts = await reader.collections.posts.all();
 	return (
 		<div className="prose dark:prose-invert">
 			<h1>Posts</h1>
+			<p>
+				Longer form content. For interesting links with limited commentary,{' '}
+				<Link href="links">check out the links section</Link> instead.
+			</p>
 			<ul>
 				{posts.map((post) => (
 					<li key={post.slug}>
