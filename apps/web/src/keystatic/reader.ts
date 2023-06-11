8014ce3a9365a492d6097fdd3f6b52515ada68df
Luke Bennett <hello@lukebennett.com.au>
2023-06-11 15:04:03 +1000

    Switch to process.cwd()

--- apps/web/src/keystatic/reader.ts
+++ apps/web/src/keystatic/reader.ts
@@ -2,4 +2,4 @@ import { createReader } from '@keystatic/core/reader';
 
 import { config } from './keystatic.config';
 
-export const reader = createReader('', config);
+export const reader = createReader(process.cwd(), config);
