a6ecd4e2c7aaf6d8ae953e8f8c264761aec31095
Luke Bennett <hello@lukebennett.com.au>
2023-06-11 16:30:57 +1000

    Fix links in feed

--- apps/web/src/app/feed.xml/route.ts
+++ apps/web/src/app/feed.xml/route.ts
@@ -7,8 +7,23 @@ import { sortPosts, toIsoString } from '~/lib/utils';
 const MAX_POSTS = 100;
 
 export async function GET() {
-	const posts = await reader.collections.posts.all();
-	const links = await reader.collections.links.all();
+	const [links, posts] = await Promise.all([
+		reader.collections.posts.all().then((posts) =>
+			posts.map((post) => ({
+				...post,
+				type: 'post' as const,
+				url: `posts/${post.slug}`,
+			}))
+		),
+		reader.collections.links.all().then((links) =>
+			links.map((link) => ({
+				...link,
+				type: 'link' as const,
+				url: `links/${link.slug}`,
+			}))
+		),
+	]);
+
 	const feed = new RSS({
 		title: siteConfig.title,
 		description: siteConfig.description,
@@ -17,17 +32,17 @@ export async function GET() {
 	});
 
 	const sortedEntries = sortPosts([...posts, ...links])
-		// @ts-ignore
-		.filter(({ entry }) => !entry.isDraft)
+		.filter(
+			(item) =>
+				item.type === 'link' || (item.type === 'post' && !item.entry.isDraft)
+		)
 		.slice(0, MAX_POSTS);
 
-	console.log(sortedEntries);
-
-	sortedEntries.forEach(async ({ entry, slug }) => {
+	sortedEntries.forEach(async ({ entry, url }) => {
 		feed.item({
 			title: entry.title,
 			description: '',
-			url: `${siteConfig.baseUrl}/${slug}`,
+			url: `${siteConfig.baseUrl}/${url}`,
 			date: toIsoString(entry.publishedAt),
 		});
 	});
