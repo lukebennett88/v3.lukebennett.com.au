02914c9c5af51136701c0c7f0cabcc5fcd80c042
Luke Bennett <hello@lukebennett.com.au>
2023-06-11 13:52:41 +1000

    Add robots.txt

---
+++ apps/web/src/app/robots.ts
@@ -0,0 +1,15 @@
+import { type MetadataRoute } from 'next';
+
+import { siteConfig } from '~/config/site';
+
+export default function robots(): MetadataRoute.Robots {
+	return {
+		rules: [
+			{
+				userAgent: '*',
+			},
+		],
+		sitemap: `${siteConfig.baseUrl}/sitemap.xml`,
+		host: siteConfig.baseUrl,
+	};
+}
