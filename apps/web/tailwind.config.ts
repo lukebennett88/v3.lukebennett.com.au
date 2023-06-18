8ff8057a3371f1a524f1567d0c398dceb842afc4
Luke Bennett <hello@lukebennett.com.au>
2023-06-18 21:36:06 +1000

    Switch tone of gray

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,4 +1,5 @@
 import { type Config } from 'tailwindcss';
+import { neutral } from 'tailwindcss/colors';
 import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
@@ -6,6 +7,9 @@ export default {
 	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
+			colors: {
+				gray: neutral,
+			},
 			fontFamily: {
 				sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
 			},
