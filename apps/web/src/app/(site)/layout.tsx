d4031df0f3b5f0aaed8cf3868ebedc2cf7774d58
Luke Bennett <hello@lukebennett.com.au>
2023-06-21 08:55:21 +1000

    Add metadataBase

--- apps/web/src/app/(site)/layout.tsx
+++ apps/web/src/app/(site)/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
 import { clsx } from 'clsx';
+import { type Metadata } from 'next';
 
 import { ErrorBoundary } from '~/components/error-boundary';
 import { Footer } from '~/components/footer';
@@ -14,7 +15,8 @@ import { fontSans } from '~/lib/fonts';
 export const metadata = {
 	title: siteConfig.title,
 	description: siteConfig.description,
-};
+	metadataBase: new URL(siteConfig.baseUrl),
+} satisfies Metadata;
 
 export default function RootLayout({
 	children,
@@ -27,7 +29,7 @@ export default function RootLayout({
 			lang="en-AU"
 			className={clsx(fontSans.variable, 'font-sans text-xl')}
 		>
-			<body className="flex min-h-[100dvh] flex-col bg-gray-100 dark:bg-gray-900 [tab-size:2]">
+			<body className="flex min-h-[100dvh] flex-col bg-gray-100 [tab-size:2] dark:bg-gray-900">
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 					<Nav />
 					<main id={MAIN_ID} className={clsx(containerClasses, 'flex-1 pb-20')}>
