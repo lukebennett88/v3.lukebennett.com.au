777e8d9cdeb9e193ad748408053e45fee8e9c2f3
Luke Bennett <hello@lukebennett.com.au>
2023-07-02 13:57:50 +1000

    Redesign (#4)

--- apps/web/src/app/(site)/page.tsx
+++ apps/web/src/app/(site)/page.tsx
@@ -5,13 +5,11 @@ import { reader } from '~/keystatic/reader';
 export default async function Page() {
 	const { content } = await reader.singletons.homepage.readOrThrow();
 	return (
-		<div className="grid items-center gap-y-8 md:grid-cols-2">
+		<div className="mx-auto grid max-w-prose items-center gap-16">
 			<div className="prose dark:prose-invert">
 				<DocumentRenderer document={await content()} />
 			</div>
-			<div>
-				<Me />
-			</div>
+			<Me className="w-full" />
 		</div>
 	);
 }
--- apps/web/src/lib/fonts.ts
+++ apps/web/src/lib/fonts.ts
@@ -1,6 +1,12 @@
-import { Atkinson_Hyperlegible } from 'next/font/google';
+import { Source_Sans_3, Source_Serif_4 } from 'next/font/google';
 
-export const fontSans = Atkinson_Hyperlegible({
+export const fontSerif = Source_Serif_4({
+	subsets: ['latin'],
+	variable: '--font-serif',
+	weight: ['400', '700'],
+});
+
+export const fontSans = Source_Sans_3({
 	subsets: ['latin'],
 	variable: '--font-sans',
 	weight: ['400', '700'],
