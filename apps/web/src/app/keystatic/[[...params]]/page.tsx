037ef38025d62429353138d5b4b157fefdab9044
Luke Bennett <hello@lukebennett.com.au>
2023-07-02 15:35:11 +1000

    Move Keystatic outside of route group

--- apps/web/src/app/keystatic/[[...params]]/page.tsx
+++ apps/web/src/app/keystatic/[[...params]]/page.tsx
@@ -1,6 +1,8 @@
 import { type Metadata } from 'next';
 
-export const metadata = { title: 'Keystatic' } satisfies Metadata;
+export const metadata = {
+	title: 'Keystatic',
+} satisfies Metadata;
 
 export default function Page() {
 	return null;
