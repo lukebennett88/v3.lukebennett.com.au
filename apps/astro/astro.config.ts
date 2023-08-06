b61badc83572732a76d52c90f91814de140bf517
Luke Bennett <hello@lukebennett.com.au>
2023-08-06 11:51:24 +1000

    WIP

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -1,9 +1,10 @@
 import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+/** @see https://astro.build/config */
 export default defineConfig({
-	integrations: [react(), markdoc()],
+	integrations: [markdoc(), react(), tailwind()],
 	output: 'hybrid',
 });
