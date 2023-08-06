50a28aed3333809e611708b7d259fcd75e683c20
Luke Bennett <hello@lukebennett.com.au>
2023-08-06 21:24:48 +1000

    WIP

--- apps/astro/src/keystatic/component-block/tweet.tsx
+++ apps/astro/src/keystatic/component-block/tweet.tsx
@@ -1,14 +1,8 @@
 import { component, fields } from '@keystatic/core';
-
-// import { Tweet } from 'react-tweet';
+import { Tweet } from 'react-tweet';
 
 export const tweet = component({
-	preview: ({ fields }) => (
-		<>
-			{fields.id.value}
-			{/* <Tweet id={fields.id.value} /> */}
-		</>
-	),
+	preview: ({ fields }) => <Tweet id={fields.id.value} />,
 	label: 'Tweet',
 	schema: {
 		id: fields.text({
