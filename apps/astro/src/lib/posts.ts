4c48396049802e03743e4c2d1e75c3d6a076d936
Luke Bennett <hello@lukebennett.com.au>
2023-08-06 21:41:08 +1000

    Add JSON feed

--- apps/astro/src/lib/posts.ts
+++ apps/astro/src/lib/posts.ts
@@ -1,3 +1,8 @@
+import { reader } from '~/keystatic/reader';
+
+/** Date v3 of the website was first published */
+export const startDate = '2023-05-29';
+
 type Post = {
 	slug: string;
 	entry: {
@@ -12,6 +17,36 @@ export function sortPosts<P extends Post>(posts: P[]) {
 			new Date(b.entry.publishedAt).getTime() -
 			new Date(a.entry.publishedAt).getTime(),
 	);
+}
+
+export async function getSortedEntries() {
+	const [links, posts] = await Promise.all([
+		reader.collections.posts.all().then((posts) =>
+			posts.map((post) => ({
+				...post,
+				type: 'post' as const,
+				pathname: `/posts/${post.slug}`,
+			})),
+		),
+		reader.collections.links.all().then((links) =>
+			links.map((link) => ({
+				...link,
+				type: 'link' as const,
+				pathname: `/links/${link.slug}`,
+			})),
+		),
+	]);
+
+	return sortPosts([...posts, ...links]).filter(
+		(item) =>
+			item.type === 'link' || (item.type === 'post' && !item.entry.isDraft),
+	);
+}
+
+export function toIsoString(date: unknown): string {
+	return new Date(typeof date === 'string' ? date : startDate)
+		.toISOString()
+		.split('T')[0]!;
 }
 
 export function formatToAustralianDate(date: string) {
