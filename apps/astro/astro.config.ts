b231565c3906fa94300a462e0935855c4209ab96
Luke Bennett <hello@lukebennett.com.au>
2023-09-10 17:53:56 +1000

    Add sitemap integration (#42)
    * Add sitemap integration
    
    * Add link to head

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -1,5 +1,6 @@
 import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
@@ -8,6 +9,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://www.lukebennett.com.au',
 	adapter: vercel(),
-	integrations: [markdoc(), react(), tailwind()],
+	integrations: [markdoc(), react(), sitemap(), tailwind()],
 	output: 'hybrid',
 });
