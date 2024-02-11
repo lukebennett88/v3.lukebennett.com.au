702ab42d8418ab8df536a46926c4ea239713260f
Luke Bennett <hello@lukebennett.com.au>
2024-02-11 14:31:50 +1100

    Fix casing of API route

--- apps/web/src/pages/api/keystatic/[...params].ts
+++ apps/web/src/pages/api/keystatic/[...params].ts
@@ -2,7 +2,7 @@ import { makeHandler } from '@keystatic/astro/api';
 
 import { config } from '~/keystatic/keystatic.config';
 
-export const all = makeHandler({
+export const ALL = makeHandler({
 	config,
 });
 
