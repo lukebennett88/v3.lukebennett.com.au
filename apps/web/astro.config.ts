6f71fb3b503f9d41c9ecc2219452024defe0dc19
Luke Bennett <hello@lukebennett.com.au>
2024-12-22 14:46:55 +1100

    Update dependencies

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -2,22 +2,19 @@ import markdoc from '@astrojs/markdoc';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 /** @see https://astro.build/config */
 export default defineConfig({
 	adapter: vercel({
 		edgeMiddleware: true,
-		speedInsights: {
-			enabled: true,
-		},
 		webAnalytics: {
 			enabled: true,
 		},
 	}),
 	integrations: [markdoc(), react(), sitemap(), tailwind()],
-	output: 'hybrid',
+	output: 'static',
 	site: 'https://www.lukebennett.com.au',
 	server: {
 		host: true,
